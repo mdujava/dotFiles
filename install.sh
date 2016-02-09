594c3e949f3302df0b8c632676ea4aed458aa8f6
Matej Dujava <mdujava@redhat.com>
2016-02-09 14:13:23 +0100

    Edit install script.
    Signed-off-by: Matej Dujava <mdujava@redhat.com>

--- install.sh
+++ install.sh
@@ -6,6 +6,10 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 ln -sv ${BASEDIR}/vim/.vimrc ~/.vimrc
 ln -sv ${BASEDIR}/vim/ ~/.vim
 
+# bash
+ln -sv ${BASEDIR}/bash/.bashrc ~/.bashrc
+ln -sv ${BASEDIR}/bash/.bash_aliases ~/.bash_aliases
+
 # zsh
 #ln -sv ${BASEDIR}/zshrc ~/.zshrc
 
