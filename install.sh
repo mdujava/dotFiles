979eeec10f9cef661ff8b27945878b64b629f0f5
Matej Dujava <mdujava@gmail.com>
2016-02-10 09:58:36 +0100

    Create installing script, add git sconfig, add vim plugins
    Signed-off-by: Matej Dujava <mdujava@gmail.com>

--- install.sh
+++ install.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
-# vim
-ln -sv ${BASEDIR}/vim/.vimrc ~/.vimrc
-ln -sv ${BASEDIR}/vim/ ~/.vim
+echo ${BASEDIR}
+echo $email
 
 # bash
 ln -sv ${BASEDIR}/bash/.bashrc ~/.bashrc
 ln -sv ${BASEDIR}/bash/.bash_aliases ~/.bash_aliases
 
+# git
+touch ${BASEDIR}/git/.gitconfig
+cat ${BASEDIR}/git/.gitconfig-head > ${BASEDIR}/git/.gitconfig
+echo '    email =' $email >> ${BASEDIR}/git/.gitconfig
+cat ${BASEDIR}/git/.gitconfig-tail >> ${BASEDIR}/git/.gitconfig
+ln -sv ${BASEDIR}/git/.gitconfig ~/.gitconfig
+
+# vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+ln -sv ${BASEDIR}/vim/.vimrc ~/.vimrc
+vim +PluginInstall +qall
+
 # zsh
 #ln -sv ${BASEDIR}/zshrc ~/.zshrc
-
-# git
-#ln -sv ${BASEDIR}/gitconfig ~/.gitconfig
--- uninstall.sh
+++ uninstall.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+echo ${BASEDIR}
 
-# vim
-rm ~/.vimrc
-rm ~/.vim
-
-# zsh
-rm ~/.zshrc
+# bash
+rm -v ~/.bashrc
+rm -v ~/.bash_aliases
 
 # git
-rm ~/.gitconfig
+rm -v ~/.gitconfig
+rm -v ${BASEDIR}/git/.gitconfig
+
+# vim
+rm -v ~/.vimrc
+echo 'removed directory: ‘/home/redhat/.vim/bundle/‘'
+rm -rf ~/.vim/bundle/
+
+# zsh
+#rm -v ~/.zshrc
